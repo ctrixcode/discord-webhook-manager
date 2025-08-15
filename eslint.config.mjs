1338e0d5302483ce15c89ea32543f2675da4053e
Chandrashekhar Kachawa <42296139+ctrixcode@users.noreply.github.com>
2025-08-15 22:11:27 +0530

    fix: lint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,7 +5,7 @@ import { defineConfig } from 'eslint/config';
 export default defineConfig([
   {
     files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
-    languageOptions: { globals: globals.browser },
+    languageOptions: { globals: globals.node },
   },
   tseslint.configs.recommended,
 ]);
