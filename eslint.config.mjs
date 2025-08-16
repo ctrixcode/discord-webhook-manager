6c5c0a843fe5f4e7fe9f214976b57de391a1ab45
Chandrashekhar Kachawa <42296139+ctrixcode@users.noreply.github.com>
2025-08-16 12:03:20 +0530

    fix: eslint underscore ignroe var

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,6 +6,16 @@ export default defineConfig([
   {
     files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
     languageOptions: { globals: globals.node },
+    rules: {
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+          caughtErrorsIgnorePattern: '^_',
+        },
+      ],
+    },
   },
   tseslint.configs.recommended,
 ]);
