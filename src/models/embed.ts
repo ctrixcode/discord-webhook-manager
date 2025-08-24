e714a3d39f5e7ea24cee9daa409e7be238db2aec
Chandrashekhar Kachawa <42296139+ctrixcode@users.noreply.github.com>
2025-08-24 11:06:13 +0530

    feat: message template model created

---
+++ src/models/embed.ts
@@ -0,0 +1,59 @@
+import { Schema, Document } from 'mongoose';
+
+interface IEmbedSchemaDocument extends Document {
+  title?: string;
+  description?: string;
+  url?: string;
+  color?: string;
+  timestamp?: Date;
+  image?: {
+    url: string;
+  };
+  thumbnail?: {
+    url: string;
+  };
+  author?: {
+    name: string;
+    url?: string;
+    icon_url?: string;
+  };
+  fields?: {
+    name: string;
+    value: string;
+    inline?: boolean;
+  }[];
+  footer?: {
+    text: string;
+    icon_url?: string;
+  };
+}
+
+export const EmbedSchema = new Schema<IEmbedSchemaDocument>({
+  title: { type: String },
+  description: { type: String },
+  url: { type: String },
+  color: { type: String },
+  timestamp: { type: Date },
+  image: {
+    url: { type: String },
+  },
+  thumbnail: {
+    url: { type: String },
+  },
+  author: {
+    name: { type: String },
+    url: { type: String },
+    icon_url: { type: String },
+  },
+  fields: [
+    {
+      name: { type: String },
+      value: { type: String },
+      inline: { type: Boolean },
+    },
+  ],
+  footer: {
+    text: { type: String },
+    icon_url: { type: String },
+  },
+});
--- src/models/message_template.ts
+++ src/models/message_template.ts
@@ -1,9 +1,42 @@
 import { Schema, model, Document } from 'mongoose';
+import { EmbedSchema } from '../models/embed';
+
+interface IEmbedInMessageTemplate {
+  title?: string;
+  description?: string;
+  url?: string;
+  color?: string;
+  timestamp?: Date;
+  image?: {
+    url: string;
+  };
+  thumbnail?: {
+    url: string;
+  };
+  author?: {
+    name: string;
+    url?: string;
+    icon_url?: string;
+  };
+  fields?: {
+    name: string;
+    value: string;
+    inline?: boolean;
+  }[];
+  footer?: {
+    text: string;
+    icon_url?: string;
+  };
+}
 
 export interface IMessageTemplate extends Document {
   user_id: Schema.Types.ObjectId;
   name: string;
-
+  description?: string;
+  content: string;
+  avatar_ref: Schema.Types.ObjectId;
+  embeds: IEmbedInMessageTemplate[];
+  attachments: string[];
   createdAt: Date;
   updatedAt: Date;
 }
@@ -11,6 +44,14 @@ export interface IMessageTemplate extends Document {
 const MessageTemplate = new Schema<IMessageTemplate>(
   {
     user_id: { type: Schema.Types.ObjectId, required: true, ref: 'User' },
+    name: { type: String, required: true },
+    description: { type: String },
+    content: { type: String, required: true },
+    avatar_ref: { type: Schema.Types.ObjectId, required: true, ref: 'Avatar' },
+    embeds: [EmbedSchema],
+    attachments: [{ type: String }],
+    createdAt: { type: Date, required: true, default: Date.now },
+    updatedAt: { type: Date, required: true, default: Date.now },
   },
   {
     timestamps: true,
