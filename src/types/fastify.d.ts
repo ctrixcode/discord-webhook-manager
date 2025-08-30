45fff6615641ecbe0f2ef3ecdf5e0b842ee1b9a0
Chandrashekhar Kachawa <42296139+ctrixcode@users.noreply.github.com>
2025-08-30 10:03:54 +0530

    fix: remove username from fastify req

--- src/types/fastify.d.ts
+++ src/types/fastify.d.ts
@@ -5,7 +5,6 @@ declare module 'fastify' {
   interface FastifyRequest {
     user?: {
       userId: string;
-      username: string;
       email: string;
       refreshTokenJti?: string; // Added for single session logout
     };
