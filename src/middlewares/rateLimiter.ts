5653d9676b14d709292a61b3e0f773e0a952c7b9
Chandrashekhar Kachawa <42296139+ctrixcode@users.noreply.github.com>
2025-09-02 14:44:47 +0530

    feat: rate limiter working

--- src/middlewares/rateLimiter.ts
+++ src/middlewares/rateLimiter.ts
@@ -4,10 +4,10 @@ import rateLimit from '@fastify/rate-limit';
 const rateLimiterPlugin = fp(async fastify => {
   await fastify.register(rateLimit, {
     max: 100,
-    timeWindow: '15 minutes',
-    errorResponseBuilder: () => ({
-      error: 'Too many requests from this IP, please try again later.',
-      retryAfter: '15 minutes',
+    timeWindow: '1 minute',
+    errorResponseBuilder: (req, context) => ({
+      error: `Too many requests from this IP, please try again after ${context.after}.`,
+      retryAfter: context.after,
     }),
   });
 });
