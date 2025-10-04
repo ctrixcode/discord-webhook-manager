51e5e1754dee86ece7bab648aa6a4f599a56f520
Chandrashekhar Kachawa <42296139+ctrixcode@users.noreply.github.com>
2025-10-04 21:36:51 +0530

    chore: removed schema for user usage

--- apps/backend/src/routes/user.routes.ts
+++ apps/backend/src/routes/user.routes.ts
@@ -2,7 +2,6 @@ import { FastifyInstance } from 'fastify';
 import * as userController from '../controllers/user.controller';
 import { authenticate } from '../middlewares';
 import { getUsersQuerySchema } from '../schemas/user.schema';
-import { userUsageResponseSchema } from '../schemas/user-usage.schema';
 
 const userRoutes = async (fastify: FastifyInstance) => {
   fastify.get(
@@ -13,12 +12,7 @@ const userRoutes = async (fastify: FastifyInstance) => {
   fastify.get(
     '/me/usage',
     {
-      preHandler: [authenticate], // Authentication is required
-      schema: {
-        response: {
-          200: userUsageResponseSchema, // Add response schema
-        },
-      },
+      preHandler: [authenticate],
     },
     userController.getUserUsageHandler
   );
