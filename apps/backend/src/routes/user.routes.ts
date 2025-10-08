d827bb7e3668a07e084c651ac7ba0fc960ac6686
anuragbansode <anuragbansode10@gmail.com>
2025-10-08 14:11:44 +0530

    docs : created Backend - user routes

--- apps/backend/src/routes/user.routes.ts
+++ apps/backend/src/routes/user.routes.ts
@@ -1,18 +1,42 @@
 import { FastifyInstance } from 'fastify';
 import * as userController from '../controllers/user.controller';
 import { authenticate } from '../middlewares';
-import { getUsersQuerySchema } from '../schemas/user.schema';
 
 const userRoutes = async (fastify: FastifyInstance) => {
   fastify.get(
     '/',
-    { schema: getUsersQuerySchema, preHandler: [authenticate] },
+    {
+      preHandler: [authenticate],
+      schema: {
+        summary: 'Get current user profile',
+        description:
+          'Retrieves the profile information for the currently authenticated user.',
+        tags: ['user'],
+        response: {
+          200: { description: 'Current user profile details.' },
+          401: { description: 'Error: Unauthorized.' },
+          500: { description: 'Error: Internal Server Error' },
+        },
+      },
+    },
     userController.getCurrentUser
   );
+
   fastify.get(
     '/me/usage',
     {
       preHandler: [authenticate],
+      schema: {
+        summary: 'Get user API usage',
+        description:
+          'Retrieves the API usage statistics for the currently authenticated user (e.g., number of webhooks, avatars).',
+        tags: ['user'],
+        response: {
+          200: { description: 'User API usage statistics.' },
+          401: { description: 'Error: Unauthorized.' },
+          500: { description: 'Error: Internal Server Error' },
+        },
+      },
     },
     userController.getUserUsageHandler
   );
