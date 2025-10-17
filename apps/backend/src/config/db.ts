8bd456046943ab7cad0350342458ab6f585da832
candykisu <uraharakisu@proton.me>
2025-10-17 15:30:39 +0530

    fix: env issues

--- apps/backend/src/config/db.ts
+++ apps/backend/src/config/db.ts
@@ -1,11 +1,11 @@
 import mongoose from 'mongoose';
 import { logger } from '../utils';
+import { env } from './env';
 
 const connectDB = async () => {
   try {
     const mongoUri =
-      process.env.MONGO_URL ||
-      'mongodb://localhost:27017/discord-webhook-manager-test';
+      env.MONGO_URL || 'mongodb://localhost:27017/discord-webhook-manager-test';
     await mongoose.connect(mongoUri);
     logger.info('MongoDB connected successfully');
   } catch (error) {
